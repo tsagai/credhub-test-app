f3676cc2f5e67ce0e36b8adf422eefa4d47b25f1
st033129 <sagai.taylor@broadcom.com>
2025-02-13 13:46:04 -0500

    updated curl

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat /usr/local/share/ca-certificates/* > ~/ca.crt
 while true; do
-    echo "Curl date: $(date)"
+    
     curl 'https://credhub.service.cf.internal:8844/api/v1/interpolate' -i -X POST \
     --capath ~/ca.crt \
     --key /etc/cf-instance-credentials/instance.key \
@@ -10,5 +10,6 @@ while true; do
     -d '{
   "test":"test" 
 }'
-    sleep 1
+    echo "Curl date: $(date)"
+    sleep 10
 done
