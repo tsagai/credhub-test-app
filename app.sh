adf561a94e2328fe3451be7c87132f74ac8a9b9b
st033129 <sagai.taylor@broadcom.com>
2025-02-13 14:06:02 -0500

    added more verbosity to curl

--- app.sh
+++ app.sh
@@ -2,7 +2,7 @@
 cat /usr/local/share/ca-certificates/* > ~/ca.crt
 while true; do
     
-    curl 'https://credhub.service.cf.internal:8844/api/v1/interpolate' -i -X POST \
+    curl 'https://credhub.service.cf.internal:8844/api/v1/interpolate' -vi -X POST \
     --capath ~/ca.crt \
     --key /etc/cf-instance-credentials/instance.key \
     --cert /etc/cf-instance-credentials/instance.crt \
